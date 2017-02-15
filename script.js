21d7164b37281328b43946bc9015daf89d3f46df
Yifei Wang <hello.yifei@icloud.com>
2017-02-15 20:05:17 +0100

    weather show

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
 window.addEventListener("load",function(event){
+	const APPID = '2d3055ddb7941ccc16f48f3aaeb29121'
 	let go = document.getElementById("go");
-	var marker;
-  let putMarkOnMap = function(event){
-    //event.preventDefault();
-    
-    let inputValue = document.getElementsByTagName("input")[0].value;
-    var geocoder = new google.maps.Geocoder();
+    let putMarkOnMap = function(event){
+    	//event.preventDefault();
+    	let marker;
+    	let inputValue = document.getElementsByTagName("input")[0].value;
+    	var geocoder = new google.maps.Geocoder();
 
-    let f =  function(results) {
-        map.setCenter(results[0].geometry.location);
-        map.fitBounds(results[0].geometry.bounds);//automatically set level
-        marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-    }
-    geocoder.geocode({"address": inputValue},f);
-  }
-  go.addEventListener("click",putMarkOnMap);
-  go.addEventListener("click",function(event){
-    let req = new XMLHttpRequest();
-    req.onreadystatechange = function(event) {
-      if( req.readyState == 4 ){
-        let res = JSON.parse(req.responseText);
-        console.log(req.responseText)
+    	let f =  function(results) {
+    		map.setCenter(results[0].geometry.location);
+        	map.fitBounds(results[0].geometry.bounds);//automatically set level
+
+        	// put marker on map
+        	marker = new google.maps.Marker({
+        		map: map,
+        		position: results[0].geometry.location
+        	});
+    	}
+    	geocoder.geocode({"address": inputValue},f);
+       // show weather info
+    	let req = new XMLHttpRequest();
+    	let url = `http://api.openweathermap.org/data/2.5/weather?q=${inputValue}&APPID=${APPID}`;
+    	req.open("get",url)
+    	req.onreadystatechange = function(event) {
+      		if( req.status == 200 && req.readyState == 4){
+        		let res = JSON.parse(req.responseText);
+        		console.log(req.responseText)
         
-        let data = `<h5 style=color:green;>${inputValue} weather:</h5><p style="color:red;">${res.name},${res.weather[0].description},wind speed: ${res.wind.speed}</p>`;
-        //let data = "<h5 style=color:green;>"+inputValue
-        let div = document.createElement('div');
-
-   }
- }
+        		let data = `weather: ${res.weather[0].main}, 
+        		description: ${res.weather[0].description}, 
+        		temperature: ${res.main.temp},
+        		wind speed: ${res.wind.speed}, 
+        		cloud: ${res.clouds.all}`;
+        		document.getElementById('weather-container').innerHTML = data;
+   			}
+ 		}
+		req.send();
+  	}
+  	go.addEventListener("click",putMarkOnMap);
+  	
+   
 });
