83cfd64801eda1f0c7c05c32106a394b5dadc812
Niklas Nauber <nauber91@hotmail.com>
2017-02-16 10:07:08 +0100

    adding every API to one file

--- WikiAPI/wikiscript.js
+++ WikiAPI/wikiscript.js
@@ -2,37 +2,14 @@ let searchbtn = document.getElementById('searchbtn');
 let searchtext = document.getElementById('searchtext');
 let searchvalue;
 let responsedisplayed = document.getElementById('response');
-searchbtn.addEventListener('click', function(){
-    searchvalue=searchtext.value
-    
-    let req = new XMLHttpRequest
-    
-        let url = "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchvalue + "&format=json&origin=*";
-    
-req.open('GET', url);
-    
-req.onreadystatechange = function(event) {
 
-	if( req.readyState == 4 )
-console.log('- success!');
-	console.log("-----");
-    let myPlaceParsed = JSON.parse(req.responseText)
-  let displayedtext= '';
     
-        
-       displayedtext+= myPlaceParsed[2] + '<br>'+'<br>'
-    responsedisplayed.innerHTML= displayedtext
-    console.log(myPlaceParsed);
     
 
-};
-
-req.send();
-    
-
-console.log('Du sökte: '+ searchvalue)
 
     
 });
+    
+
 
 
