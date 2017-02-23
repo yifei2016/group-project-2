851d7841b1ae7ade0641f5631e6af8ecae9bf7eb
Louice Danielsson <louice.danielsson@gmail.com>
2017-02-23 15:21:58 +0100

    last commit before merge w sebbe

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ window.addEventListener("load", function (event) {
   // go.addEventListener('click',function(){
   //     $('#myModal').modal('toggle');
        
-  // });
+  //});
   go.addEventListener("click", putMarkOnMap);
 });
 
