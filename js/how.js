8d77db89d9bc690d8a1362d35e6552b493e46e66
Yifei Wang <hello.yifei@icloud.com>
2017-02-28 20:55:18 +0100

    final

---
+++ js/how.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+
+  console.log("dfsdf")
+  let ask = document.getElementById("ask");
+  let center = document.getElementsByClassName("center")[0];
+
+  ask.addEventListener("click", function() {
+
+    let answer = document.getElementsByClassName("answer")[0];
+    // answer.style.top = "0px";
+    center.style.visibility = "visible";
+    let close = document.getElementById("close");
+    close.addEventListener("click", function() {
+      center.style.visibility = "hidden";
+    })
+  })
+}
