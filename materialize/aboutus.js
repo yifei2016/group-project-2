1ac191af1b496ad7a61beda4f26072ef7a9ec81a
Yifei Wang <hello.yifei@icloud.com>
2017-02-28 12:39:18 +0100

    aout

--- materialize/aboutus.js
+++ materialize/aboutus.js
@@ -1,15 +1,16 @@
-window.onload = function(){
+let myFunction = function(){
+console.log("sdfdsf")
 	let ask = document.getElementById("ask");
 	let center = document.getElementsByClassName("center")[0];
+
 	ask.addEventListener("click",function(){
+
 	let answer = document.getElementById("answer");
 	answer.style.top = "0px";
 	center.style.visibility="visible";
-	})
-
 	let close = document.getElementById("close");
 	close.addEventListener("click",function(){
-		
 		center.style.visibility="hidden";
 	})
+})
 }
